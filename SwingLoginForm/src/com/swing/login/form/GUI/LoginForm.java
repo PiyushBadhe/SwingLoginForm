cf69dd8a4cefde748fdd3effd174c0207de383e4
PiyushBadhe <badhepiyuraj1997@gmail.com>
2023-01-15 18:55:10 +0530

    Created frame and added components to it

--- SwingLoginForm/src/com/swing/login/form/GUI/LoginForm.java
+++ SwingLoginForm/src/com/swing/login/form/GUI/LoginForm.java
@@ -1,13 +1,16 @@
 package com.swing.login.form.GUI;
 
+import java.awt.GridLayout;
+
 import javax.swing.JButton;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 public class LoginForm {
 
-	// Method to place components
+		// Method to place components
 		public static void addComponentsToPanel (JPanel panel) {
 			
 			JLabel userLabel = new JLabel("Username");
@@ -30,6 +33,20 @@ public class LoginForm {
 	
 	
 	public static void main(String[] args) {
+		
+		// Java Frame
+		JFrame frame = new JFrame ("Login Form");
+		// Using GridLayout to simplify the UI
+		JPanel panel = new JPanel(new GridLayout(3, 2, 15, 15)); // (int rows, int cols, int hgap, int vgap)
+		
+		// Calling the method addComponentsToPanel
+		addComponentsToPanel(panel);
+		
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  // Close and Exit
+		frame.setSize(300, 150);
+		frame.setLocationRelativeTo(null);  // Positioned the frame in the center of the screen
+		frame.getContentPane().add(panel);	// Added component panel to the Content Pane
+		frame.setVisible(true);				// Makes the frame appear on the screen when run
 
 	}
 
