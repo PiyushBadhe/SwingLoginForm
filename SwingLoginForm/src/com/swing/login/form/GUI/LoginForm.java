b6bb7e703bc9d4810bc8374610bff5109de7533e
PiyushBadhe <badhepiyuraj1997@gmail.com>
2023-01-15 21:31:01 +0530

    Added respective Actions to the buttons

--- SwingLoginForm/src/com/swing/login/form/GUI/LoginForm.java
+++ SwingLoginForm/src/com/swing/login/form/GUI/LoginForm.java
@@ -1,10 +1,13 @@
 package com.swing.login.form.GUI;
 
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
@@ -37,13 +40,46 @@ public class LoginForm {
 		panel.add(passwordText);
 		panel.add(cancelButton);
 		panel.add(loginButton);
+		
+		// Adding Action to buttons
+		loginButton.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if (checkData(userText.getText(), passwordText.getText())) {
+					JOptionPane.showMessageDialog(panel, "Hello Mr. " + userText.getText() +
+					" Welcome to the Application!", "Login Successful", JOptionPane.INFORMATION_MESSAGE);
+				}
+				else {
+					JOptionPane.showMessageDialog(panel, "Invalid username or password",
+							"Login Error", JOptionPane.ERROR_MESSAGE);
+				}
+			}
+		});
+		
+		cancelButton.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				JOptionPane.showMessageDialog(cancelButton, "Login Cancelled by the user",
+						"Login Termination", JOptionPane.OK_CANCEL_OPTION);
+				System.exit(0);
+			}
+		});
+		
+		
 			
 		/*
+		 #1 
 		  	- Our goal is to develop a login module that authenticates users based on a predefined username and password
 		 	- Hence adding actions or functionalities to the login and cancel buttons
 		 	- First, creating a static boolean method first hand (before adding Components) to check whether the username and password is correct or not
 		 	- Meaning, when user enters username and password and hit login button, firstly this checkData() method
 		 	  will check the data and if its correct, it will authenticate the user and displays a message else an error message will be shown
+		 #2	
+		 	- Now as we captured and analyzed the data whether its correct or not, adding action to the buttons is only remaining task to be done
+		 	- By using .addActionListener(); we can bring those buttons into action
+		 	- And by using JOptionPane class, we can show message dialogs for successful, unsuccessful logins and also a terminating message 
 		 */
 			
 		}
